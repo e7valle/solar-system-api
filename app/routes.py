e2040f393614a6bc9ee13fc8013390527b91502c
Elizabeth Valle <e7valle@gmail.com>
2023-04-20 11:53:18 -0700

    Wave 1- created class Planet

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,32 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify 
 
+class Planet:
+    def __init__(self, id, name, description, moons, color):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.moons = moons
+        self.color = color
+
+planet1 = Planet(1, "Mercury", "Mercury is hot", 0, "gray")
+planet2 = Planet(2, "Venus", "No moons here", 0, "yellow")
+planet3 = Planet(3, "Earth", "Life is good here", 1, "blue-green")
+
+planet_list = [planet1, planet2, planet3]
+
+planet_bp = Blueprint("planet", __name__, url_prefix="/planet")
+
+@planet_bp.route("", methods=["GET"])
+def get_planets():
+    response = []
+    for planet in planet_list:
+        planet_dict = {
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "moons": planet.moons,
+            "color": planet.color
+        }
+        response.append(planet_dict)
+
+    return jsonify(response), 200 
